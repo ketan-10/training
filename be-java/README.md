a3302515667a53f8877a02371da39547a0ccf482
ketan-10 <ketanchaudhari1998@gmail.com>
2023-12-05 02:04:18 +0530

    Kubernetes wait for other service to be ready before web deployment to start

--- be-java/README.md
+++ be-java/README.md
@@ -1,1 +1,7 @@
 # Backend
+
+- > `kubectl explain pod.spec.containers.readinessProbe`
+- > `kubectl explain pod.spec.containers.readinessProbe --recursive`
+- > `kubectl describe deploy/web`
+- > `kubectl logs deploy/web`
+- > `kubectl get pods --help`
--- be-java/do.sh
+++ be-java/do.sh
@@ -10,6 +10,12 @@ if [[ $1 = 'run' ]]; then
     run
 elif [[ $1 = 'build' ]]; then
     build
+elif [[ $1 == 'docker' ]]; then
+    # https://stackoverflow.com/questions/75089403/docker-exec-usr-local-openjdk-11-bin-java-exec-format-error
+    docker buildx build --platform linux/amd64 -t backend-image . 
+    registoryPath=us-central1-docker.pkg.dev/elite-firefly-401018/internal-repo/backend-image:$2
+    docker tag backend-image $registoryPath
+    docker push $registoryPath
 else
     echo "Usage Not found"
     exit 2
