72c7c2fd3690b3c8fdadf51fbc7904816b93ab99
ketan-10 <ketanchaudhari1998@gmail.com>
2023-12-10 21:38:55 +0530

    Changed Dockerfile -- now maven build happens in Dockerfile

--- rest-be/src/main/java/com/ketan/training/config/ApplicationConfig.java
+++ rest-be/src/main/java/com/ketan/training/config/ApplicationConfig.java
@@ -46,7 +46,7 @@ public class ApplicationConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
